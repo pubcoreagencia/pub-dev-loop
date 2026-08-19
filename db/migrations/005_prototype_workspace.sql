ca3f21b5b1342bb6f26964beda87b17ca163de36
pubcoreagencia <contato.pubcore@gmail.com>
2026-08-19 12:21:18 -0300

    chore(prototype): remove duplicate workspace migration

--- db/migrations/005_prototype_workspace.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE prototype_sessions
-  ADD COLUMN IF NOT EXISTS workspace_path TEXT;
-
-CREATE INDEX IF NOT EXISTS prototype_sessions_workspace_idx
-  ON prototype_sessions (workspace_path)
-  WHERE workspace_path IS NOT NULL;
